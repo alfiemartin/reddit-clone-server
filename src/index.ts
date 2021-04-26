eeb8de56588c218b2de9701057a18f2ea1a135bb
alfiemartin <76059804+alfiemartin@users.noreply.github.com>
2021-04-26 10:20:53 +0100

    more setup Post entity added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __PROD__ } from "./constants";
+import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
+
+  const post = orm.em.create(Post, { title: "My First Post" });
+  await orm.em.persistAndFlush(post);
+  console.log(__dirname);
 };
 
 main();
-console.log("hedllo");
